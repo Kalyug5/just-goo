8eaa8f3f93d14d98522cc20b8685bdeb94976172
Priyanshu <mishrapriyanshu555@gmail.com>
2024-08-03 18:54:06 +0530

    "updated"

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Println("Go Lang Project")
 
 
-	if os.Getenv("GO_ENV") != "production" {
+	if os.Getenv("ENV") != "production" {
 		err := godotenv.Load(".env")
 		if err != nil {
 			log.Fatal("Error loading .env file")
